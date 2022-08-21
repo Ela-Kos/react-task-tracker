3f9731a5a429fb869c2bbec76ee6dd009bd556d2
ElaK <103433676+Ela-Kos@users.noreply.github.com>
2022-08-21 19:11:28 +0100

    upload completed tutorial files

---
+++ src/App.js
@@ -0,0 +1,73 @@
+// Add import React from "react"; for Class with Components
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(true);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors apt",
+      day: "Feb 5th at 12.30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Job interview",
+      day: "Feb 6th at 10am",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food shopping",
+      day: "Feb 5th at 5pm",
+      reminder: false,
+    },
+  ]);
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  return (
+    <div className="container">
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to Show"
+      )}
+    </div>
+  );
+};
+// using class Components...
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello</h1>;
+//   }
+// }
+
+export default App;
